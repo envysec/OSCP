7f1ac03914a29d88a7c3b06c3778ee4a75ada182
envysec <50002925+envysec@users.noreply.github.com>
2019-06-27 15:13:14 -0400

    Added Knock Script
    Added knock script

---
+++ Scripts/portKnock.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#This script takes target ip and ports as input to knock
+#Once port knocking is complete an nmap scan will run to check open ports
+
+#Check for number of arguments
+if [ $# -lt 3 ]; then
+	echo "[*] Syntax: ./portKnock.sh [ip] {[port 1] [port 2] .. [port3]}"
+	exit 0
+fi
+
+#Save target ip and shift arguments
+ip=$1
+shift
+
+#Port Knocking
+for port in #@; do
+	echo "[*] Knocking on port $port"
+	nmap -Pn --host-timeout 201 --max-retries 0 -p $port $ip | grep "Nmap done:" && sleep 1
+done
+
+echo "[*] Scanning for open ports on $ip"
+
+#Scan for open ports
+nmap -Pn $ip
